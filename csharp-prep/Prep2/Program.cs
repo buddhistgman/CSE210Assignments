4c513d6e9dae69615d81d1c09ad15e72142b9794
Alexander Torrente <aatorrente@gmail.com>
2023-01-07 21:13:46 -0600

    here is prep 1 & prep 2 for week 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string rawgrade = Console.ReadLine();
+        int grade = int.Parse(rawgrade);
+        string letterGrade;
+        
+        if (grade >= 90) letterGrade = "A";
+        else if (grade >=80 && grade <=89) letterGrade = "B";
+        else if (grade >=70 && grade <=79) letterGrade = "C";
+        else if (grade >=60 && grade <=69) letterGrade = "D";
+        else letterGrade = "F";
+        
+        string sign = "";
+        int firstnumber = grade % 10;
+        
+        if (firstnumber >= 7) sign = "+";
+        else if (firstnumber < 3) sign = "-";
+        else sign = "";
+        
+        Console.WriteLine("Your grade is a " + letterGrade + sign + "!");
+        
+        if (grade >= 70) Console.WriteLine("You passed the course!");
+        else Console.WriteLine("You failed the course!");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your last name?  ");
+        string LastName = Console.ReadLine();
+        Console.WriteLine ("Your name is " + LastName + ", " + FirstName + " " + LastName + ".");
     }
 }
