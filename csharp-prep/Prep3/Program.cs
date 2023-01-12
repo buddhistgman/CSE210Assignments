3ac2e5d8ef14acca7ca2f1489978f893f0f64b5a
Alexander Torrente <aatorrente@gmail.com>
2023-01-11 23:52:13 -0600

    Prep 3 and 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,47 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep3 World!");
+    Random randomGenerator = new Random();
+    int number = randomGenerator.Next(1, 11);
+      
+    int guess = -1;
+    int numGuess = 0;
+    string input = "";
+    do
+    {    
+        do //(guess != number)
+        {
+            Console.Write ("you are at " + numGuess + " guesses.... \n");
+            Console.Write ("guess a number, between 1 and 10: ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess > number)
+            {
+                Console.Write ("Lower,  ");
+                numGuess = numGuess + 1;
+            }
+            
+            else if (guess < number)
+            {
+                Console.Write ("Higher, ");
+                numGuess = numGuess + 1;
+            }
+            
+            else
+            {
+            Console.WriteLine ("Correct!, you got the answer correct after " + numGuess + " guesses!");
+            
+                
+            }
+            
+        }while (guess != number);
+    Console.Write("Do you want to continue? ");
+    numGuess = 0;
+    number = randomGenerator.Next(1, 11);
+    input = Console.ReadLine();    
     }
+    while (input == "yes");
+}
 }
